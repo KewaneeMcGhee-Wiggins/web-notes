b58fe2958c053c92fe0fe5676c3e7605567ef64c
KewaneeMcGhee-Wiggins <47463588+KewaneeMcGhee-Wiggins@users.noreply.github.com>
2019-05-22 20:43:40 -0400

    3rd Commit

--- app.js
+++ app.js
@@ -7,28 +7,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const myNotes = [
     'http is a protocol',
     'http requests have a url, method, header, and body',
-    'remember your syntax',
+    'remember your syntax'
 ];
 
 app.post('/notes', (req, res) => { 
-    notes.push(req.body.note);
+    myNotes.push(req.body.notes);
+    console.log(myNotes);
     res.redirect('/');
   });
+ 
+ 
   
-  
-app.use('/css', express.static
-('css'));
-//app.get('/', (_req, res) => res.send('Web Notes'))
+app.use('/css', express.static('css'));
+//view w/css
+// app.get('/', (_req, res) => res.send('Web-Notes'));
 //app.use('/', express.static('views'));
+//more dynamic view
 app.get('/', (req, res) => {
     res.render('notes.ejs', { notes: myNotes });
-});
+})   
 
-app.listen(3000, (req,res) =>
+
+app.listen(3000, (req,res) =>{
     console.log(`Example app listening on port 3000 !`)
-    )
     
-
-
+})
 
 
